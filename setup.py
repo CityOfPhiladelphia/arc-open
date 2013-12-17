a5a04b01b3b20c9094cc1671c48cb10110d89352
Dave Walk <daviddwalk@gmail.com>
2013-12-17 15:56:13 -0500

    Bumping up to v1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='ArcOpen',
-	    version='0.3',
+	    version='1.0.0',
 	    description='An ArcGIS toolbox to convert feature classes to open geodata formats',
 	    author='City of Philadelphia',
 	    author_email='data@phila.gov',
